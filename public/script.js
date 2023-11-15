f72a1e0f715b0aa4a666ee6b1550484b8ceb5b1d
ranbast <ranouil.bastien@gmail.com>
2023-11-15 16:27:02 +0100

    Filtre ajouté et fonctionnel, affichage des appartements  fonctionnel

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 $(function(){
-    let dateRangePicker = document.getElementById('dateEntree');
+    let dateRangePicker = document.getElementById('dateSejour');
     let pickerRange = new Litepicker({
         element: dateRangePicker,
         format: 'DD MMMM YYYY',
@@ -10,3 +10,19 @@ $(function(){
 
 
 //Librairie importée dans le HTML via une balise <script> qui permet d'afficher un calendrier pour sélectionner des dates 
+
+
+
+const prix = document.querySelector('#priceRange');
+const paraPrix = document.querySelector(".prix");
+
+prix.addEventListener('change', function(e){
+    paraPrix.innerHTML = ": " + e.target.value + "€";
+})
+
+const distance = document.querySelector('#distanceRange');
+const paraDist = document.querySelector(".distance");
+
+distance.addEventListener('change', function(e){
+    paraDist.innerHTML = ": " + e.target.value + " m"
+})
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const mysql = require('mysql');
-const ejs = require('ejs');
 const app = express();
 const path = require('path');
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname,'public')));
 
 const con = mysql.createConnection({
     host: "localhost",
@@ -17,17 +17,112 @@ con.connect(function (err){
     console.log("Connected!");
 })
 
-app.get('/',(req,res) => {
-    res.render('/views/index');
+
+app.get('/', async (req,res) => {
+    try{
+    const address = req.query.address;
+    const dateSejour = req.query.dateSejour;
+    const prix = req.query.price
+    const nbrChambre = req.query.nbrChambre;
+    const nbrBed = req.query.nbrBed;
+    const distance = req.query.distance;
+
+    const biens = await fetchBiens(address,dateSejour,prix,nbrChambre,nbrBed,distance); 
+    res.render('index', {data : biens});
+
+    
+
+    }catch (error){
+        console.error("Erreur lors de la récupération des biens:", error);
+        res.status(500).send("Erreur lors de la récupération des biens");
+    }
+
+
+});
+
+const fetchBiens = (address,dateSejour,prix,nbrChambre,nbrBed,distance) => {
+    return new Promise((resolve, reject) => {
+
+        let sql = "SELECT *, AVG(locations.note) as moyenneNote, COUNT(locations.note) as nbrNotes FROM biens NATURAL JOIN locations WHERE 1";
+        const params = [];
+
+        if (address){
+            sql += " AND commune = ? "
+            params.push(address);
+        }
+
+        if (dateSejour){
+            sql += " AND dateSejour = ?";
+            params.push(dateSejour);
+        }
+
+        if (prix != "0"){
+            sql += " AND prix <= ?";
+            params.push(prix);
+        }
+
+        if (nbrChambre){
+            sql += " AND nbChambres <= ?"
+            params.push(nbrChambre);
+        }
+
+        if (nbrBed){
+            sql += " AND nbCouchages <= ?"
+            params.push(nbrBed);
+        }
+
+        if(distance){
+            sql += " AND distance <= ?"
+            params.push(distance);
+        }
+
+        console.log("Requête SQL:", sql);
+        console.log("Paramètres:", params);
+
+
+         con.query(sql + " group by biens.idBien", params, function(err, result) {
+             if (err) {
+                  reject(err);
+             } else {
+                  resolve(result);
+             }
+        });
+    })
+}
+
+
+app.get("/bien/:idBien", async (req,res) => {
+    try{
+        
+        
+        const dataBien = await fetchDatabyIdBien(req.params.idBien);
+        res.render('bien', {data: dataBien});
+    }catch (error){
+        console.error("Erreur lors de la récupération des données du biens:", error);
+        res.status(500).send("Erreur lors de la récupération des données du biens");
+    }
+
     
 })
 
-app.get('/'), function(req, res){
-    con.query("SELECT * FROM Biens", function(err,result){
-    if (err) throw (err);
-    res.render('/views/index',{data: result});
-});
+const fetchDatabyIdBien = (idBien) => {
+    return new Promise((reject, resolve) =>{
+        let sql = "SELECT * FROM biens WHERE idBien =?"
+        sql += idBien
+
+        con.query(sql, function(err,result){
+            if (err){
+                reject (err);
+            }else{
+                resolve(result);
+            }
+        });
+
+    })
 }
+
 
 app.listen(8888);
 console.log("Listening on port 8888");
+
+
